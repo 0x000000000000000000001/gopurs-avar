6088940a142c67f335ddc5c480ac37b319838b23
0x1 <kevin.francart3@gmail.com>
2026-07-24 16:04:45 +0200

    chore: sync package.json purescript-npm updates

---
+++ src/Effect/AVar.go
@@ -0,0 +1,41 @@
+
+
+func Empty() any {
+	panic("Not implemented: empty")
+}
+
+func _NewVar(_ any) any {
+	panic("Not implemented: _newVar")
+}
+
+func _KillVar(_ any, _ any, _ any) any {
+	panic("Not implemented: _killVar")
+}
+
+func _PutVar(_ any, _ any, _ any, _ any) any {
+	panic("Not implemented: _putVar")
+}
+
+func _TryPutVar(_ any, _ any, _ any) any {
+	panic("Not implemented: _tryPutVar")
+}
+
+func _TakeVar(_ any, _ any, _ any) any {
+	panic("Not implemented: _takeVar")
+}
+
+func _TryTakeVar(_ any, _ any) any {
+	panic("Not implemented: _tryTakeVar")
+}
+
+func _ReadVar(_ any, _ any, _ any) any {
+	panic("Not implemented: _readVar")
+}
+
+func _TryReadVar(_ any, _ any) any {
+	panic("Not implemented: _tryReadVar")
+}
+
+func _Status(_ any, _ any) any {
+	panic("Not implemented: _status")
+}
