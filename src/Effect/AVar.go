65b26418daac798f0589de8cc577a4a34f86365b
0x1 <kevin.francart3@gmail.com>
2026-07-26 16:08:30 +0200

    Auto-commit before architecture refactoring

--- src/Effect/AVar.go
+++ src/Effect/AVar.go
@@ -1,41 +1,41 @@
 
 
-func Empty() any {
+func Empty() interface{} {
 	panic("Not implemented: empty")
 }
 
-func _NewVar(_ any) any {
+func _NewVar(_ interface{}) interface{} {
 	panic("Not implemented: _newVar")
 }
 
-func _KillVar(_ any, _ any, _ any) any {
+func _KillVar(_ interface{}, _ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _killVar")
 }
 
-func _PutVar(_ any, _ any, _ any, _ any) any {
+func _PutVar(_ interface{}, _ interface{}, _ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _putVar")
 }
 
-func _TryPutVar(_ any, _ any, _ any) any {
+func _TryPutVar(_ interface{}, _ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _tryPutVar")
 }
 
-func _TakeVar(_ any, _ any, _ any) any {
+func _TakeVar(_ interface{}, _ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _takeVar")
 }
 
-func _TryTakeVar(_ any, _ any) any {
+func _TryTakeVar(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _tryTakeVar")
 }
 
-func _ReadVar(_ any, _ any, _ any) any {
+func _ReadVar(_ interface{}, _ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _readVar")
 }
 
-func _TryReadVar(_ any, _ any) any {
+func _TryReadVar(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _tryReadVar")
 }
 
-func _Status(_ any, _ any) any {
+func _Status(_ interface{}, _ interface{}) interface{} {
 	panic("Not implemented: _status")
 }
